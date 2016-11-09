8bcc75dc24169a2d7dfc89bc4b2fc0ea5d164182
Eduardo Moura <edoura@gmail.com>
2016-11-09 17:40:30 -0200

    configuração firebase - pronto para implementar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,20 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { PolymerElement } from '@vaadin/angular2-polymer';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 
 // Shared component
 import { ToolbarHomeComponent } from './shared/toolbar-home/toolbar-home.component';
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: "AIzaSyD4Suuox8Mpv4qgWlpEijD8yin-71E7JnI",
+  authDomain: "ongbook-oficial.firebaseapp.com",
+  databaseURL: "https://ongbook-oficial.firebaseio.com",
+  storageBucket: "ongbook-oficial.appspot.com"
+};
 
 @NgModule({
   declarations: [
@@ -21,7 +30,8 @@ import { ToolbarHomeComponent } from './shared/toolbar-home/toolbar-home.compone
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent],
